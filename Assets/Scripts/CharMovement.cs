bd342595f31bf65bf90ae056e5d1a7d918f4586a
SteffinieGolder <1502286@uad.ac.uk>
2022-12-04 20:24:07 +0000

    Map Colliders
    Map edges have colliders

--- Assets/Scripts/CharMovement.cs
+++ Assets/Scripts/CharMovement.cs
@@ -7,6 +7,7 @@ public class CharMovement : MonoBehaviour
     public float moveSpeed;
     //Should I get this from gameobject?
     public Animator animator;
+    private Vector3 direction;
 
     // Update is called once per frame
     void Update()
@@ -15,9 +16,13 @@ public class CharMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         //Normalising this vector prevents faster movement in diagonal direction. 
-        Vector3 direction = new Vector3(horizontal, vertical,0).normalized;
+        direction = new Vector3(horizontal, vertical,0).normalized;
 
         AnimateMovement(direction);
+    }
+
+    private void FixedUpdate()
+    {
         transform.position += direction * moveSpeed * Time.deltaTime;
     }
 
