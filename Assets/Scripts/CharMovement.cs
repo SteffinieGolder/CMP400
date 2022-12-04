ee924ebc00d6053b1bf215b3944f5a4147e7d597
SteffinieGolder <1502286@uad.ac.uk>
2022-12-04 00:02:58 +0000

    Character sprite and animation
    Set up sprite sheets, first character has walking animations

--- Assets/Scripts/CharMovement.cs
+++ Assets/Scripts/CharMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CharMovement : MonoBehaviour
 {
     public float moveSpeed;
+    //Should I get this from gameobject?
+    public Animator animator;
+
     // Update is called once per frame
     void Update()
     {
@@ -13,6 +16,26 @@ public class CharMovement : MonoBehaviour
 
         //Normalising this vector prevents faster movement in diagonal direction. 
         Vector3 direction = new Vector3(horizontal, vertical).normalized;
+
+        AnimateMovement(direction);
         transform.position += direction * moveSpeed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 movedir)
+    {
+        if(animator!=null)
+        {
+            if(movedir.magnitude>0)
+            {
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", movedir.x);
+                animator.SetFloat("vertical", movedir.y);
+            }
+
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
